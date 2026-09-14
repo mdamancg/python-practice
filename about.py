3e21871b2c44219874882483efa47ca83af3378c
mdamancg <mdaman.cg@gmail.com>
2026-09-14 15:15:01 +0530

    all hw done

--- about.py
+++ about.py
@@ -21,13 +21,47 @@
 # num=num//10
 # print(a+b+num)
 
-sum=0
-for i in range (1,100):
-    sum=sum+i
-    average=sum/99
-    print(average)
+# sum=0
+# for i in range (1,100):
+#     sum=sum+i
+#     average=sum/99
+#     print(average)
 
-number=int(input("Enter your number:"))
-for i in range(number):
-    if i%2==0:
-        print(f"{i}number is even")
+# number=int(input("Enter your number:"))
+# for i in range(number):
+#     if i%2==0:
+#         print(f"{i}number is even")
+
+# str=input("Enter a string:").strip().lower()
+# str2=""
+# length=len(str)
+# for element in range (length-1,-1,-1):
+#     str2=str2+str[element]
+# if str==str2:
+#     print("String is palindrome")
+# else:
+#     print("String is not palindrome")
+
+# number=input("Enter number:").strip()
+# number2=""
+# length=len(number)
+# for i in range (length-1,-1,-1):
+#     number2=number2+number[i]
+
+# if number==number2:
+#         print("Number is palindrome")
+# else:
+#         print("Number is not palindrome")
+
+# name = "Python"
+
+# for character in name:
+#     print(character)
+
+# word="python"
+# count=0
+
+# for character in word:
+#      count=count+1
+# print("characters:",count)
+
--- demo.py
+++ demo.py
@@ -1,7 +1,27 @@
-has_id=input("Enter only Yes/No: ").strip().lower()
-if has_id=="no":
-    print("Bring your id")
-elif has_id=="yes":
-    print("Welcome")
-else:
-    print("Enter valid number")
+# has_id=input("Enter only Yes/No: ").strip().lower()
+# if has_id=="no":
+#     print("Bring your id")
+# elif has_id=="yes":
+#     print("Welcome")
+# else:
+#     print("Enter valid number")
+
+# str = input("Enter a string: ")
+
+# if str == str[::-1]:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+
+# number=input("Enter a number: ")
+# if number==number[::-1]:
+#     print("Number is palindrome")
+# else:
+#     print("Number is not palindrome")
+
+
+for row in range(3):
+    for column in range(4):
+        print("*",end="")
+    print()
